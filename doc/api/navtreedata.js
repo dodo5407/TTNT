b05d3ed44019762f9f8cc7394401f03fdd64c76c
dodo5407 <a138949@gmail.com>
2018-09-19 22:47:21 +0800

    omnet5.3 first

---
+++ doc/api/navtreedata.js
@@ -0,0 +1,45 @@
+var NAVTREE =
+[
+  [ "OMNeT++ Simulation Library", "index.html", [
+    [ "OMNeT++ API Reference", "index.html", null ],
+    [ "API Changes", "APIChanges.html", null ],
+    [ "Modules", "modules.html", "modules" ],
+    [ "Classes", "annotated.html", [
+      [ "Class List", "annotated.html", "annotated_dup" ],
+      [ "Class Hierarchy", "hierarchy.html", "hierarchy" ],
+      [ "Class Members", "functions.html", [
+        [ "All", "functions.html", "functions_dup" ],
+        [ "Functions", "functions_func.html", "functions_func" ],
+        [ "Variables", "functions_vars.html", null ],
+        [ "Typedefs", "functions_type.html", null ],
+        [ "Enumerations", "functions_enum.html", null ],
+        [ "Enumerator", "functions_eval.html", null ]
+      ] ]
+    ] ],
+    [ "Files", null, [
+      [ "File List", "files.html", "files" ]
+    ] ]
+  ] ]
+];
+
+var NAVTREEINDEX =
+[
+".html",
+"classomnetpp_1_1cAutoRangeHistogramStrategy.html#a8ab0745de7fefce72d67f99f3931a3e4",
+"classomnetpp_1_1cComponent.html#a56b1aacbc65126ccd7cede0521eee59d",
+"classomnetpp_1_1cDisplayString.html#aebaf78e62ec56f567726b553f25a9997",
+"classomnetpp_1_1cExpression.html#a928df96c60d65fb8020fdd24e2fbc1b3",
+"classomnetpp_1_1cIOutputScalarManager.html#ac3f8273ae98126597b2235425d17ceb1",
+"classomnetpp_1_1cMessage.html#a93b289901ceadeabd25bba862d0a4440",
+"classomnetpp_1_1cNedMathFunction.html#ab2a20c2f6b7497ffc1b27cc8da48bade",
+"classomnetpp_1_1cOwnedObject.html",
+"classomnetpp_1_1cPathFigure.html#acf98d1dcc33d23cf6ab402669c2ee649",
+"classomnetpp_1_1cQueue.html#a479432127ee77145cc19d6a2d1590821",
+"classomnetpp_1_1cSimulation.html#ac3d1e884989e2e2f65ffd1bf62d495b7",
+"classomnetpp_1_1cVarHistogram.html#aa65f3b21a9a92ff022e435a7304126d2aae4c1a13f036331cc5b83835e67d3d2b",
+"functions_w.html",
+"structomnetpp_1_1cFigure_1_1Transform.html"
+];
+
+var SYNCONMSG = 'click to disable panel synchronisation';
+var SYNCOFFMSG = 'click to enable panel synchronisation';
---
+++ ide/plugins/org.omnetpp.doc_5.3.0.180411-87513e3/content/api/classomnetpp_1_1cDynamicExpression_1_1Elem.js
@@ -0,0 +1,17 @@
+var classomnetpp_1_1cDynamicExpression_1_1Elem =
+[
+    [ "operator=", "classomnetpp_1_1cDynamicExpression_1_1Elem.html#a1675f54f73e810804fc6a9cefd243683", null ],
+    [ "operator=", "classomnetpp_1_1cDynamicExpression_1_1Elem.html#a204e816fb78d6a687b39b27166944cee", null ],
+    [ "operator=", "classomnetpp_1_1cDynamicExpression_1_1Elem.html#a7c41c13c18c3c4dc23d32c1415c030b5", null ],
+    [ "operator=", "classomnetpp_1_1cDynamicExpression_1_1Elem.html#a3dab11322d084b858ff0c87bf41aa7cd", null ],
+    [ "setUnit", "classomnetpp_1_1cDynamicExpression_1_1Elem.html#a8add283e2d27370777568f19cddae03e", null ],
+    [ "operator=", "classomnetpp_1_1cDynamicExpression_1_1Elem.html#ad2bbdc3a52cc3605a20ef42fecea61eb", null ],
+    [ "operator=", "classomnetpp_1_1cDynamicExpression_1_1Elem.html#a08dbd0af4b66c76600d8539232055c7f", null ],
+    [ "operator=", "classomnetpp_1_1cDynamicExpression_1_1Elem.html#a5329b4797c9982af0abb11d1058887fe", null ],
+    [ "set", "classomnetpp_1_1cDynamicExpression_1_1Elem.html#a30bdf3b617e7c7b031339a1525e9015a", null ],
+    [ "operator=", "classomnetpp_1_1cDynamicExpression_1_1Elem.html#aa5e24a856e46f6fa17d3ac93a8a76a76", null ],
+    [ "operator=", "classomnetpp_1_1cDynamicExpression_1_1Elem.html#ae28f7c8d688c50c9710358158a1e711a", null ],
+    [ "operator=", "classomnetpp_1_1cDynamicExpression_1_1Elem.html#aa226546f31b30f82d165b563c2b37307", null ],
+    [ "compare", "classomnetpp_1_1cDynamicExpression_1_1Elem.html#a22c762b0238fa42f38fcf963fdba1b6d", null ],
+    [ "str", "classomnetpp_1_1cDynamicExpression_1_1Elem.html#a1b9b8885b0880fc4ddf9a2c7d1ca3dc4", null ]
+];
